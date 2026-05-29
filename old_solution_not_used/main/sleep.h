eefe28082aa4b5b4e0ef9e7967ad617328557ecc
domkow78 <dominik.kowalczyk@bshg.com>
2026-05-29 15:39:25 +0200

    remove old solution from repo

--- old_solution_not_used/main/sleep.h
+++
@@ -1,9 +0,0 @@
-#ifndef SLEEP_H
-#define SLEEP_H
-
-  #define uS_TO_S 1000000
-  #define uS_TO_mS 1000
-  void SleepTimerCbk(unsigned long aT);
-  void SleepRSTCbk(unsigned long aT);
-
-#endif
--- old_solution_not_used/main/taskMeasure.h
+++
@@ -1,9 +0,0 @@
-#ifndef TASK_MEASURE_H
-#define TASK_MEASURE_H
-
-#define TASK_MEASURE_HEAP 3000
-#define TASK_MEASURE_DEF_PERIOD_MS 5000
-void taskMeasureCbk(void* ptr);
-extern int taskMeasurePeriodMs;
-
-#endif
